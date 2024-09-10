c9b4fb5b2e1c2fe314ae4bd7e4d09198798fc4ea
SD <santiagod@cxnpl.com>
2024-09-10 16:27:39 -0300

    Creates route for NextJS dynamic robots.txt

--- packages/codegen/src/build/collect-public.ts
+++ packages/codegen/src/build/collect-public.ts
@@ -8,13 +8,23 @@ const { stat } = fse;
  * @returns A string array of all the files in the public folder
  */
 const collectPublicFiles = async () => {
+  console.log("collecting public files");
   const publicFiles = (await glob("public/**/*")).map((path) => path.slice(6));
   try {
     if ((await stat("app/favicon.ico")).isFile())
       publicFiles.push("/favicon.ico");
   } catch {
+    /*empty*/
+  }
+  //NextJS dynamic robots.txt
+  try {
+    if ((await stat("app/robots.ts")).isFile()) {
+      publicFiles.push("/robots.txt");
+    }
+  } catch {
     /* empty */
   }
+
   return publicFiles;
 };
 
